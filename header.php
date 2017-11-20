13336d3cc75636c2992de58258a3d4fcb9e5706b
Sanjit Misra <smisra@prysm.com>
2017-11-20 17:46:45 +0530

    Landing Page Ready

---
+++ header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+<head>
+    <title><?= isset($PageTitle) ? $PageTitle : "Default Title"?></title>
+    <!-- Additional tags here -->
+    <?php if (function_exists('customPageHeader')){
+      customPageHeader();
+    }?>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Leavermann</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="css/isotope.css" media="screen" />
+  <link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.css">
+  <link rel="stylesheet" href="css/style.css">
+  <!-- skin -->
+  <link rel="stylesheet" href="skin/default.css">
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+	$PageTitle="Leavermann - Landing";
+	function customPageHeader(){?>
+  		<!--Arbitrary HTML Tags-->
+	<?php }
+	include_once('header.php');
+?>
+
+<!-- The Body Content Begins Here-->
+<section id="header" class="appear" style="background-color: rgb(240,248,255)">
+   <div class = "row" style="padding-top:15px;">
+					   
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> 
+        </button>
+     		
+        <div class="form-group col-sm-6" ></div>	
+    
+        <div class="form-group col-sm-6" >
+	        <div class="form-group col-sm-3" >
+	            <input type="text" class="form-control" id="uname" placeholder="Id">
+
+	        </div>
+	        <div class="form-group name2 col-sm-3">
+	            <input type="Password" class="form-control" id="pwd" placeholder="Password">
+	        </div>
+
+	        <div class="form-group name2 col-sm-3">
+	            <button type="button" class="btn btn-primary">Log In</button>
+	        </div>
+	        <div class="form-group name2 col-sm-3">
+	            <button type="button" class="btn btn-primary" style="background-color: rgb(0,0,0); ">Sign Up</button>
+	        </div>
+		</div>	      
+	</div>
+</section>
+
+<section class="featured" style="background-color: rgb(65,105,225)">
+      <div class="container" >
+        <div class="row mar-bot40" >
+          <div class="col-md-6 col-md-offset-3">
+
+            <div class="align-center">
+              <i class="fa fa-flask fa-5x mar-bot20"></i>
+              <h2 class="slogan">Leavermann</h2>
+              <p>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+</section>
+
+<!-- services -->
+<section id="section-services" class="section pad-bot30 bg-white">
+      <div class="container">
+
+        <div class="row mar-bot40">
+          <div class="col-lg-4"> 
+           <div class="align-center"> 
+              <i class="fa fa-rocket fa-5x"></i> 
+              <p>Super fast deployment and live roll out. No more waiting on an endless queue.Get ahead, get quick!!
+              </p>
+            </div> 
+          </div> 
+
+          <div class="col-lg-4"> 
+            <div class="align-center">
+              <i class="fa fa-envelope fa-5x"></i> 
+              <p>Get alerts for every activity that happens. Never stay out of touch!!
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="align-center">
+              <i class="fa fa-key fa-5x"></i>
+              <p>Security right from the word go. Integrate with existing identity solutions or subscribe to our custom services. No more server breaches!!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+</section>
+<!-- The Body Content Ends Here-->
+
+<?php
+ include_once('footer.php');
+?>
