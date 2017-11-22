ca0945a77fa6749a0ee962b52fda56b5821f8180
Sanjit Misra <misra.sanjit@gmail.com>
2017-11-22 13:17:22 +0530

    Change page header based on auth status - fix2

--- signin.php
+++ signin.php
@@ -6,13 +6,12 @@
 			$PageTitle="Authenticated";
 		}
 		else
-		{ $PageTitle="Failed Auth";}
-		
-		?>
-		<?php 
-   }
-		include_once('header.php');
-?>
+		{ 
+			$PageTitle="Failed Auth";
+		}	
+    }
+ ?>
+<?php include_once('header.php'); ?>
 
 <?php 
 	if ($_POST["username"] == "rikers" && $_POST["password"] == "2017")  
