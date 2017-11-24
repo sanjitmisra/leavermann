4ccacae115233bffe43eed97f7b954d087b19968
Sanjit Misra <misra.sanjit@gmail.com>
2017-11-24 15:04:30 +0530

    debug

--- ViewLogic/signin.php
+++ ViewLogic/signin.php
@@ -1,5 +1,5 @@
 <?php
-	
+	echo $_SERVER['DOCUMENT_ROOT'];
 	include( $_SERVER['DOCUMENT_ROOT'] . '/Leavermann/DataLogic/ConnectDb.php');
 		
 	if ($_POST["username"] == "rikers" && $_POST["password"] == "2017")  
