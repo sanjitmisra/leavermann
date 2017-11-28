9689d02d4c35d71e453c42c49414269b1cc87a21
Sanjit Misra <misra.sanjit@gmail.com>
2017-11-28 15:37:21 +0530

    minor

--- businesslogic/auth/credential_check.php
+++ businesslogic/auth/credential_check.php
@@ -6,16 +6,6 @@
 	include( $_SERVER['DOCUMENT_ROOT'] . '/leavermann/datalogic/ConnectDb.php');
 
 	$isAuthenicated = ((authenticateUser($username, $password) == 1) ? 1 : 0);
-	
-/*
-	if (authenticateUser($username, $password) == 1)  
-	{
-		$isAuthenicated = 1;
-	}
-	else
-	{ 
-		$isAuthenicated = 0;
-	}	*/
-	
+
 	echo $isAuthenicated;
 ?>
